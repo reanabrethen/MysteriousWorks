bdf44582da695a318d9af11997ba2cc916966e55
Admin <reanajb@gmail.com>
2024-03-04 12:05:39 -0500

    updated music feature to automatically play when site is loaded

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
-const chicagoArtMuseumAPI = "https://api.artic.edu/api/v1/artworks"
-
+const chicagoArtMuseumAPI = "https://api.artic.edu/api/v1/artworks?fields=id,title,artist_title,date_display,main_reference_number,medium_display,place_of_origin"
 const artImages = document.querySelector('#art-display')
 
+window.addEventListener("DOMContentLoaded", () =>{
+    const museumMusic = new Audio('sounds/Liebestraum.mp3')
+    museumMusic.volume = 0.1
+    museumMusic.play()
+})
 
 
+
+
+
+
+// // const museumMusic = new Audio('sounds/Liebestraum.mp3').play()
+// const museumMusic = document.querySelector('#museum-music').play()
+// museumMusic.autoplay = true
+// museumMusic.play()
 
 fetch(chicagoArtMuseumAPI)
     .then((response)=>{
@@ -13,6 +25,6 @@ fetch(chicagoArtMuseumAPI)
     })
     .then((artObj)=>{
         console.log('response processed')
-        console.log(artObj.message)
-        artImages.src = artObj.message
+        // console.log(artObj.message)
+        // artImages.src = artObj.message
     })
