b2e64f3f9de7512b3fe303c34301741dfc0560a2
Admin <reanajb@gmail.com>
2024-03-04 12:32:33 -0500

    successfully fetched data from API

--- main.js
+++ main.js
@@ -1,30 +1,34 @@
-const chicagoArtMuseumAPI = "https://api.artic.edu/api/v1/artworks?fields=id,title,artist_title,date_display,main_reference_number,medium_display,place_of_origin"
-const artImages = document.querySelector('#art-display')
 
-window.addEventListener("DOMContentLoaded", () =>{
+
+window.addEventListener("click", () =>{
     const museumMusic = new Audio('sounds/Liebestraum.mp3')
     museumMusic.volume = 0.1
     museumMusic.play()
 })
 
+const nextButton = document.querySelector('.next')
 
 
-
-
-
-// // const museumMusic = new Audio('sounds/Liebestraum.mp3').play()
-// const museumMusic = document.querySelector('#museum-music').play()
-// museumMusic.autoplay = true
-// museumMusic.play()
-
-fetch(chicagoArtMuseumAPI)
-    .then((response)=>{
-        console.log('response received')
-        console.log(response)
-        return response.json()
+// nextButton.addEventListener('click', ()=>{
+//     const artImages = document.querySelector('#art-display')
+//     const title = document.querySelector('.title')
+//     const dateDisplay = document.querySelector('.date-display')
+//     const artistName = document.querySelector('.artist-title')
+//     const placeOfOrgin = document.querySelector('.place-of-orgin')
+//     const artMedium = document.querySelector('.medium-display')
+    
+    const chicagoArtMuseumAPI = `https://api.artic.edu/api/v1/artworks?fields=id,title,artist_title,date_display,main_reference_number,medium_display,place_of_origin`
+    
+    fetch(chicagoArtMuseumAPI)
+    .then((information)=>{
+        console.log('information received')
+        console.log(information)
+        return information.json()   //promise
     })
     .then((artObj)=>{
         console.log('response processed')
-        // console.log(artObj.message)
+        console.log(artObj)
         // artImages.src = artObj.message
     })
+// })
+
